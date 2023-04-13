dc15b74accff9dd49befbd4e14233e4d45adddd4
paseihon <paseihon@MUR-6ZTPBK3.usersad.everis.int>
2023-04-13 13:21:02 +0200

    Refactorización realizada

--- src/Player.java
+++ src/Player.java
@@ -1,49 +1,48 @@
+
 import java.awt.Color;
 
-
 public class Player {
-	private String name;
-	private int playerPosn;
-	private int playerScore;
-	private Color playerColor;
-	
 
-	public Player(int no){
-	    name = "Player " + no;
-	    playerPosn = 0;
-	}
-	
-	public int getPosition(){
-		return playerPosn;
-	}
-	
+    private String name;
+    private int playerPosn;
+    private int playerScore;
+    private Color playerColor;
 
-	public String getName(){
-		return name;
-	}
-	
-	public void setPosition(int posn){
-		playerPosn = posn;
-	}
-	
-	public void incPosition(int posn){
-		playerPosn += posn;
-	}
-	
-	public void setPlayerColor(Color c){
-		playerColor = c;
-	}
-	
-	public Color getPlayerColor(){
-		return playerColor;
-	}
-	
-	public void incPlayerScore(int a){
-		playerScore += a;
-	}
-	
-	public int getPlayerScore(){
-		return playerScore;
-	}
+    public Player(int no) {
+        name = "Player " + no;
+        playerPosn = 0;
+    }
+
+    public int getPosition() {
+        return playerPosn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPosition(int posn) {
+        playerPosn = posn;
+    }
+
+    public void incPosition(int posn) {
+        playerPosn += posn;
+    }
+
+    public void setPlayerColor(Color c) {
+        playerColor = c;
+    }
+
+    public Color getPlayerColor() {
+        return playerColor;
+    }
+
+    public void incPlayerScore(int a) {
+        playerScore += a;
+    }
+
+    public int getPlayerScore() {
+        return playerScore;
+    }
 
 }
